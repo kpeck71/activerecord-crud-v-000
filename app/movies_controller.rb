d427451bd559d818ad8e92fc80a69a01cd55a8a0
Kristen Peck <kpeck71@gmail.com>
2017-12-01 02:13:49 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,14 +1,8 @@
-# Replace the '__' in the below methods to to make the specs pass!
-# Each '__' corresponds to a single line of code you will need to write.
-# See the example below on the left and how it should look on the right.
-# def make_a_new_movie_instance    # def make_a_new_movie_instance
-#   movie = __                     #   movie = Movie.new
-# end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -19,57 +13,63 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.create(attributes)
 end
 
 def can_be_created_in_a_block
-  Movie.create do |m|
-    __
+  movie = Movie.create do |m|
+    m.title = "Home Alone"
+    m.release_date = 1990
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first.title
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last.title
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.where("id = ?",1)
+        #Movie.where("id = ?", id).first
+        #  `id` is not available from within an example (e.g. an `it` block) or from constructs that run in the scope of an example (e.g. `before`, `let`, etc). It
+       #is only available on an example group (e.g. a `describe` or `context` block).
 end
 
 def can_find_by_multiple_attributes
-  # title == "Title"
+  #title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  movies = Movie.where("title = ? and release_date = ? and director = ?", "Title", 2000, "Me")
+  movies
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  movies = Movie.where("release_date > 2002").order(release_date: :desc)
+  movies
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  new_movie = Movie.find_by(title: "Awesome Flick")
+  new_movie.title = "Even Awesomer Flick"
+  new_movie.save
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.find_by(title: "Wat?")
+  movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
@@ -77,7 +77,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.update_all title: "A Movie"
 end
 
 def can_destroy_a_single_item
@@ -92,11 +92,3 @@ def can_destroy_all_items_at_once
   end
   __
 end
-
-
-
-
-
-
-
-
