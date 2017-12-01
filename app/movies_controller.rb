4da4bcba0f9938c2976c9f6f440f519c58629878
Kristen Peck <kpeck71@gmail.com>
2017-12-01 02:24:23 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -37,7 +37,6 @@ end
 
 def can_find_the_first_item_from_the_database_using_id
   Movie.all.where("id = ?",1)
-  binding.pry
         #Movie.where("id = ?", id).first
         #  `id` is not available from within an example (e.g. an `it` block) or from constructs that run in the scope of an example (e.g. `before`, `let`, etc). It
        #is only available on an example group (e.g. a `describe` or `context` block).
